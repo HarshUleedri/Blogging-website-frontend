af56fa8874e1f6afd0044d93089f4012d332306d
HarshUleedri <ulidari2@gmail.com>
2025-05-16 01:35:06 +0530

    feat: :bug: removed unused imports

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from "./App.jsx";
 import Store from "./app/store";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { SearchProvider } from "./context/searchContext/searchContext.jsx";
 
 const queryClient = new QueryClient();
 
@@ -13,9 +12,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={Store}>
-        <SearchProvider>
-          <App />
-        </SearchProvider>
+        <App />
       </Provider>
     </QueryClientProvider>
   </StrictMode>
